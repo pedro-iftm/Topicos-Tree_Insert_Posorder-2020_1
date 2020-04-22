fe874d9597765ca5428ee06c28bcb4a5d5f44aea
Pedro Santos Arruda <pedro.arruda@morpheustecnologia.com>
2020-04-21 22:41:42 -0300

    Add get height and get positive numbers

--- tree.py
+++ tree.py
@@ -5,9 +5,16 @@ class Tree:
     def __init__(self, root_value, node_values):
         self.root = Node(root_value)
         self.insert(node_values)
+        self.positive_numbers = []
 
     def __call__(self):
-        self.pos_order(self.root)
+        print('pos order')
+        self.pos_order(self.root, True)
+        print('\nheight')
+        height = self.get_height(self.root)
+        print(height)
+        print('\npositive numbers')
+        print(self.positive_numbers)
 
     def insert(self, node_values):
         for value in node_values:
@@ -24,6 +31,7 @@ class Tree:
                         actual_node = new_node
                         last_node.left = new_node
                         break
+
                 else:
                     actual_node = last_node.right
 
@@ -32,10 +40,26 @@ class Tree:
                         last_node.right = new_node
                         break
 
-    def pos_order(self, actual_node):
+    def pos_order(self, actual_node, save_positive_numbers=False):
         if actual_node == None:
             return
 
-        self.pos_order(actual_node.left)
-        self.pos_order(actual_node.right)
+        self.pos_order(actual_node.left, save_positive_numbers)
+        self.pos_order(actual_node.right, save_positive_numbers)
         print(actual_node.value)
+
+        if save_positive_numbers:
+            self.__set_positive_numbers(actual_node.value)
+
+    def __set_positive_numbers(self, value):
+        if value >= 0:
+            self.positive_numbers.append(value)
+
+    def get_height(self, actual_node):
+        if actual_node == None or actual_node.left == None and actual_node.right == None:
+            return 0
+
+        else:
+            return 1 + (self.get_height(actual_node.left)
+                        if self.get_height(actual_node.left) > self.get_height(actual_node.right)
+                        else self.get_height(actual_node.right))
